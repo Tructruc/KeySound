31f339a67bfc5aa07ab7602c8dd2d644f1539021
Tructruc <emi.fieu.81@gmail.com>
2021-07-14 17:59:10 +0200

    Initalising the repositoty

---
+++ main.py
@@ -0,0 +1,43 @@
+from pygame import mixer
+from pynput.keyboard import Key, Listener, Controller
+import threading
+from tkinter import *
+
+
+class Detector:
+    def __init__(self):
+        mixer.init()
+        self.sound = mixer.Sound("sounds/winchester.mp3")
+
+    def show(self, key):
+
+        self.sound.play()
+
+        if key == Key.esc:
+            # Stop listener
+            return False
+
+    def detect(self):
+        with Listener(on_press=self.show) as listener:
+            listener.join()
+
+
+def main():
+    screen = Tk()  # initialize
+    screen.geometry("500x500")  # pixels
+    screen.title("Sound Key")
+    screen.configure(bg='white')  # hex colors or normal colors
+    running_label = Label(screen, text="Sound Key\n is running !", font=("Courrier", 44))
+    running_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+    quit_info_label = Label(screen, text="For stop Sound Key, just close this window")
+    quit_info_label.pack(side=BOTTOM)
+    screen.mainloop()
+
+
+if __name__ == '__main__':
+    exit_flag = False
+    detector_thread = threading.Thread(target=Detector().detect)
+    detector_thread.start()
+    main()
+    keyboard = Controller()
+    keyboard.press(Key.esc)
